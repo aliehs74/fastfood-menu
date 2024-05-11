3fc4999a02f0607e17c5e6af90d078de6e1a88c1
aliehs74 <fe.afra@gmail.com>
2024-05-11 16:07:06 +0330

    init

---
+++ components/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import headerBackground from "../public/images/header-background.jpg";
+const Hero = () => {
+  return (
+    <div className=" w-full mx-auto h-32 relative ">
+      <Image
+        src={headerBackground}
+        fill={true}
+        alt="header background picture"
+        objectFit="cover"
+      />
+      <div className="absolute left-[10vw] bottom-16">
+        <h1 className="font-morabbaBold text-gray-200 text-3xl uppercase">
+          fastfood menu
+        </h1>{" "}
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      danaNormal: "danaNormal",
+      danaBold: "danaBold",
+      morabbaNormal: "morabbaNormal",
+      morabbaBold: "morabbaBold",
     },
+    extend: {},
   },
   plugins: [],
 };
